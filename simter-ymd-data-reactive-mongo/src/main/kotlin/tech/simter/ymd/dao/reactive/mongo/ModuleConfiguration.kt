ad93888fd896548897894c4d6ec25149772b6740
RJ <rongjihuang@gmail.com>
2018-12-05 16:20:42 +0800

    Initial data-reactive-mongo module

---
+++ simter-ymd-data-reactive-mongo/src/main/kotlin/tech/simter/ymd/dao/reactive/mongo/ModuleConfiguration.kt
@@ -0,0 +1,33 @@
+package tech.simter.ymd.dao.reactive.mongo
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+
+private const val MODULE_PACKAGE = "tech.simter.ymd.dao.reactive.mongo"
+
+/**
+ * All configuration for this module.
+ *
+ * @author RJ
+ */
+@Configuration("$MODULE_PACKAGE.ModuleConfiguration")
+@EnableReactiveMongoRepositories(MODULE_PACKAGE)
+@ComponentScan(MODULE_PACKAGE)
+@EntityScan(basePackages = ["tech.simter.ymd.po"])
+class ModuleConfiguration {
+  private val logger = LoggerFactory.getLogger(ModuleConfiguration::class.java)
+  @Bean
+  @ConditionalOnProperty(name = ["simter.mongodb.enabled-logging-event-listener"], havingValue = "true")
+  @ConditionalOnMissingBean
+  fun mongoEventListener(): LoggingEventListener {
+    logger.warn("instance a LoggingEventListener bean for mongodb operations")
+    return LoggingEventListener()
+  }
+}
