29664aeff7567f0a070d23925348aae900bea936
RJ <rongjihuang@gmail.com>
2018-12-05 15:43:35 +0800

    Create sql script for postgres

---
+++ simter-ymd-data/src/main/resources/tech/simter/ymd/sql/postgres/schema-create.sql
@@ -0,0 +1,16 @@
+/**
+ * Create table script.
+ * @author RJ
+ */
+create table st_ymd (
+  type  varchar(100) not null,
+  year  smallint     not null,
+  month smallint     not null default 0,
+  day   smallint     not null default 0,
+  primary key (type, year, month, day)
+);
+comment on table  st_ymd       is 'the date of massive data';
+comment on column st_ymd.type  is 'the belong type identity';
+comment on column st_ymd.year  is '4 digits year, such as 2018';
+comment on column st_ymd.month is 'month from 1 to 12. 0 means ignored';
+comment on column st_ymd.day   is 'day from 1 to 31. 0 means ignored';
