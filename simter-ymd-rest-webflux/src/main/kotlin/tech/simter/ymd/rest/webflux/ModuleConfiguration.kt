9ae3e156439d8847923b2b8d469db9214b8acfa1
SX <965600079@qq.com>
2018-12-21 17:23:38 +0800

    Implements rest  `/{type}/month?y=...`

--- simter-ymd-rest-webflux/src/main/kotlin/tech/simter/ymd/rest/webflux/ModuleConfiguration.kt
+++ simter-ymd-rest-webflux/src/main/kotlin/tech/simter/ymd/rest/webflux/ModuleConfiguration.kt
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType.TEXT_PLAIN
 import org.springframework.web.reactive.function.server.router
+import tech.simter.ymd.rest.webflux.handler.FindDaysHandler
+import tech.simter.ymd.rest.webflux.handler.FindMonthsHandler
 import tech.simter.ymd.rest.webflux.handler.FindYearsHandler
 
 private const val MODULE = "tech.simter.ymd.rest.webflux"
@@ -26,6 +28,7 @@ private const val MODULE = "tech.simter.ymd.rest.webflux"
 class ModuleConfiguration @Autowired constructor(
   @Value("\${module.version.simter-ymd:UNKNOWN}") private val version: String,
   @Value("\${module.rest-context-path.simter-ymd:/ymd}") private val contextPath: String,
+  private val findMonthsHandler: FindMonthsHandler,
   private val findYearsHandler: FindYearsHandler
 ) {
   private val logger = LoggerFactory.getLogger(ModuleConfiguration::class.java)
@@ -40,6 +43,8 @@ class ModuleConfiguration @Autowired constructor(
   @ConditionalOnMissingBean(name = ["$MODULE.Routes"])
   fun simterYmdRoutes() = router {
     contextPath.nest {
+      // GET /{type}/month Find all months of the specific type and year
+      FindMonthsHandler.REQUEST_PREDICATE.invoke(findMonthsHandler::handle)
       // GET /{type}/year  Find all years of the specific type
       FindYearsHandler.REQUEST_PREDICATE.invoke(findYearsHandler::handle)
       // GET /
