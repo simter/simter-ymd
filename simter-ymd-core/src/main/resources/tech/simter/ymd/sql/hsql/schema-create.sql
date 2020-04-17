dd9080cb3833d3584c304a1cf44a28d49c70e579
RJ <rongjihuang@gmail.com>
2020-04-17 19:55:10 +0800

    Add hsql SQL

---
+++ simter-ymd-core/src/main/resources/tech/simter/ymd/sql/hsql/schema-create.sql
@@ -0,0 +1,13 @@
+create table st_ymd (
+  id varchar(36)        primary key,
+  t  varchar(100)       not null,
+  y  smallint           not null,
+  m  smallint default 0 not null,
+  d  smallint default 0 not null,
+  constraint st_ymd_uid unique (t, y, m, d)
+);
+comment on table st_ymd is 'the date of massive data';
+comment on column st_ymd.t is 'the belong type identity';
+comment on column st_ymd.y is '4 digits year, such as 2018';
+comment on column st_ymd.m is 'month from 1 to 12. 0 means ignored';
+comment on column st_ymd.d is 'day from 1 to 31. 0 means ignored';
