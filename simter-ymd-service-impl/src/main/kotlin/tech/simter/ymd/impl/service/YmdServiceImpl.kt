fec132833e26c658f6e3ab17fb5ad287a0cf6ebd
RJ <rongjihuang@gmail.com>
2020-12-23 13:39:43 +0800

    Add `@Transactional` config on `YmdServiceImpl`

--- simter-ymd-service-impl/src/main/kotlin/tech/simter/ymd/impl/service/YmdServiceImpl.kt
+++ simter-ymd-service-impl/src/main/kotlin/tech/simter/ymd/impl/service/YmdServiceImpl.kt
@@ -3,6 +3,7 @@ package tech.simter.ymd.impl.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import tech.simter.reactive.security.ModuleAuthorizer
@@ -25,30 +26,35 @@ class YmdServiceImpl @Autowired constructor(
   private val moduleAuthorizer: ModuleAuthorizer,
   private val dao: YmdDao
 ) : YmdService {
+  @Transactional(readOnly = false)
   override fun create(vararg ymd: Ymd): Mono<Void> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_SAVE).then(
       dao.create(*ymd)
     )
   }
 
+  @Transactional(readOnly = true)
   override fun findYears(type: String): Flux<Int> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ).thenMany(
       dao.findYears(type)
     )
   }
 
+  @Transactional(readOnly = true)
   override fun findMonths(type: String, year: Int): Flux<Int> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ).thenMany(
       dao.findMonths(type, year)
     )
   }
 
+  @Transactional(readOnly = true)
   override fun findDays(type: String, year: Int, month: Int): Flux<Int> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ).thenMany(
       dao.findDays(type, year, month)
     )
   }
 
+  @Transactional(readOnly = true)
   override fun findYearsWithLatestYearMonths(type: String): Flux<YearWithItsMonths> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ).thenMany(
       dao.findYears(type)
@@ -71,6 +77,7 @@ class YmdServiceImpl @Autowired constructor(
     )
   }
 
+  @Transactional(readOnly = true)
   override fun findMonthsWithLatestMonthDays(type: String, year: Int): Flux<MonthWithItsDays> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ).thenMany(
       findMonthsWithLatestMonthDays2(type, year)
@@ -97,6 +104,7 @@ class YmdServiceImpl @Autowired constructor(
       }.flatMapIterable { it }
   }
 
+  @Transactional(readOnly = true)
   override fun findYearsWithLatestMonthDays(type: String): Flux<YearWithItsMonthDays> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ).thenMany(
       dao.findYears(type)
