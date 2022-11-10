6a3894edca58ebdd7bbdec75953540859e23fcd9
RJ <rongjihuang@gmail.com>
2022-11-10 13:10:06 +0800

    Fixed r2dbc dao implementation

--- simter-ymd-dao-r2dbc/src/test/kotlin/tech/simter/ymd/impl/dao/r2dbc/FindYearsTest.kt
+++ simter-ymd-dao-r2dbc/src/test/kotlin/tech/simter/ymd/impl/dao/r2dbc/FindYearsTest.kt
@@ -1,13 +1,12 @@
 package tech.simter.ymd.impl.dao.r2dbc
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
-import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import reactor.kotlin.test.test
 import tech.simter.ymd.core.YmdDao
-import tech.simter.ymd.impl.dao.r2dbc.TestHelper.insert
 import tech.simter.ymd.test.TestHelper.randomType
 import tech.simter.ymd.test.TestHelper.randomYmd
 
@@ -18,10 +17,15 @@ import tech.simter.ymd.test.TestHelper.randomYmd
  */
 @SpringJUnitConfig(UnitTestConfiguration::class)
 @DataR2dbcTest
-class FindYearsMethodImplTest @Autowired constructor(
-  private val client: DatabaseClient,
+class FindYearsTest @Autowired constructor(
+  private val helper: TestHelper,
   private val dao: YmdDao
 ) {
+  @BeforeEach
+  fun clean() {
+    helper.clean().test().verifyComplete()
+  }
+
   @Test
   fun `Found nothing`() {
     dao.findYears(type = randomType())
@@ -36,7 +40,7 @@ class FindYearsMethodImplTest @Autowired constructor(
     val t1y2 = randomYmd(type = t1y1.type, year = 2001)
     val t2y1 = randomYmd(type = randomType(), year = 2003) // another type
     val all = listOf(t1y1, t1y2, t2y1)
-    all.forEach { insert(client = client, ymd = it) }
+    dao.create(*all.toTypedArray()).test().verifyComplete()
 
     // invoke and verify with desc order
     dao.findYears(type = t1y1.type)
